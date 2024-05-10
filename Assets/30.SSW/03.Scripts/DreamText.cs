f1537f30b56f3fa157974efa3fee6c7fe1371b30
swsong0607 <85236025+swsong0607@users.noreply.github.com>
2024-05-11 06:11:35 +0900

    GameStory

---
+++ Assets/30.SSW/03.Scripts/DreamText.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using System;
+using System.Xml.Serialization;
+using UnityEngine.SceneManagement;
+
+
+public class DreamText : MonoBehaviour
+{
+public TMP_Text tutorialTxt;
+
+    //public Text text;
+
+    string dialogue;
+
+    public String[] tutorialDialogue;
+
+    public String[] dialogues;
+
+    void Start()
+    {
+        StartTalk(tutorialDialogue);
+    }
+
+   IEnumerator Typing (String talk)
+   {
+    //텍스트를 null 값으로 설정
+    tutorialTxt.text = null;
+    tutorialTxt.DOText(talk, 4f, true, ScrambleMode.Numerals).SetDelay(2); // 모든 문자가 출력되는데 걸리는 시간 = N초 
+
+    // 다음 대사 딜레이 
+    yield return new WaitForSeconds(4.0f); // 딜레이 N초
+    NextTalk();
+   }
+
+   //다음 대사 출력을 위한 정수 
+    public int talkNum;
+
+    public void StartTalk(String[] talks)
+    {
+        dialogues = talks;
+
+        //tutoNum 번째 대사 출력 
+        StartCoroutine(Typing(dialogues[talkNum]));
+    }
+
+    public void NextTalk()
+    {
+        tutorialTxt.text = null;
+        // 다음 배열 출력하기 위해 +1
+        talkNum++;
+
+        //tutoNum이 배열의 길이랑 같으면 끝내기
+        if(talkNum == dialogues.Length)
+        {
+            EndTalk();
+            return;
+            
+            
+        }
+
+        StartCoroutine(Typing(dialogues[talkNum]));
+    }
+
+    public void EndTalk()
+    {
+        //tutoNum 초기화 
+        talkNum = 0;
+        Debug.Log("자막 끝!"); 
+
+        IEnumerator loadScene() 
+    {
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene("TestScene0510_2100");
+    }
+    }
+     
+
+   
+}
+        
---
+++ Assets/30.SSW/03.Scripts/StroyText.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using System;
+using System.Xml.Serialization;
+using UnityEngine.SceneManagement;
+public class StroyText : MonoBehaviour
+{
+
+    
+    public TMP_Text tutorialTxt;
+
+    //public Text text;
+
+    string dialogue;
+
+    public String[] tutorialDialogue;
+
+    public String[] dialogues;
+
+    void Start()
+    {
+        StartTalk(tutorialDialogue);
+    }
+
+   IEnumerator Typing (String talk)
+   {
+    //텍스트를 null 값으로 설정
+    tutorialTxt.text = null;
+    tutorialTxt.DOText(talk, 6f); // 모든 문자가 출력되는데 걸리는 시간 = 1초 
+
+    // 다음 대사 딜레이 
+    yield return new WaitForSeconds(8.0f);
+    NextTalk();
+   }
+
+   //다음 대사 출력을 위한 정수 
+    public int talkNum;
+
+    public void StartTalk(String[] talks)
+    {
+        dialogues = talks;
+
+        //tutoNum 번째 대사 출력 
+        StartCoroutine(Typing(dialogues[talkNum]));
+    }
+
+    public void NextTalk()
+    {
+        tutorialTxt.text = null;
+        // 다음 배열 출력하기 위해 +1
+        talkNum++;
+
+        //tutoNum이 배열의 길이랑 같으면 끝내기
+        if(talkNum == dialogues.Length)
+        {
+            EndTalk();
+            return;
+        }
+
+        StartCoroutine(Typing(dialogues[talkNum]));
+    }
+
+    public void EndTalk()
+    {
+        //tutoNum 초기화 
+        talkNum = 0;
+        Debug.Log("자막 끝!");
+        SceneManager.LoadScene("DreamScene");
+
+        
+    }
+
+    /*public static void TMPDOText(TextMeshProUGUI text, float duration) //듀레이션은 대사가 출력되는데 걸리는 시간 
+    {
+        text.maxVisibleCharacters = 0;
+        DOTween.To(X => text.maxVisibleCharacters = (int)X, 0f, text.text.Length, duration);
+    }
+    
+    public TextMeshProUGUI text2;
+    */
+   
+}

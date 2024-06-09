e89e0cae42afbbb0b6490ab9328d129c0f07653c
haesuilson <shson724@naver.com>
2024-06-09 16:10:30 +0900

    포탈, 재화UI, 포션UI, 점프대 등

--- Assets/00.Test/Scripts/MapEvent/JumpPlatform.cs
+++ Assets/00.Test/Scripts/MapEvent/JumpPlatform.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class JumpPlatform : MonoBehaviour
 {
+    private Animator animator;
     Rigidbody2D rb;
     public int JumpPower = 10;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,11 +25,16 @@ public class JumpPlatform : MonoBehaviour
             // Determine if the "Player" GameObject has Rigidbody2D components
             if (playerRb != null)
             {
+                animator.SetBool("BangStart", true);
                 // Apply jumping force to "Player" GameObject
                 Vector2 jumpPowerVector = new Vector2(0, JumpPower);
                 playerRb.AddForce(jumpPowerVector, ForceMode2D.Impulse);
             }
         }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        animator.SetBool("BangStart", false);
     }
 
 }
